93e192994c2e0fdd57d7840cd3d20b15a4d8f7e6
a <a@a.com>
2026-03-08 02:07:59 -0500

    Fix : ModuleConfig's -> Scanning in the config

--- Backend/src/notifications/src/main/java/com/arka/notifications/NotificationsModuleConfig.java
+++ Backend/src/notifications/src/main/java/com/arka/notifications/NotificationsModuleConfig.java
@@ -1,10 +1,15 @@
 package com.arka.notifications;
 
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;  
 import org.springframework.context.annotation.Configuration;  
+
   
 @Configuration  
 @ComponentScan(basePackages = "com.arka.notifications")  
+
+
+@EntityScan(basePackages = "com.arka.notifications")  
 public class NotificationsModuleConfig {  
 }
