b4675addb3aca5520723bc70822d472697872b5a
a <a@a.com>
2026-03-08 01:18:46 -0500

    add :  ( E2E Tests | IntegrationTests ) : Se finaliza la suite de test el primer y mas pequeño modulo del aplicativo.
    Se generó la estructura base de reutilizacion para futuras suitesTest en otros modulos

--- Backend/src/bootstrap/src/main/java/com/arka/bootstrap/ArkaAplication.java
+++ Backend/src/bootstrap/src/main/java/com/arka/bootstrap/ArkaAplication.java
@@ -2,10 +2,8 @@ package com.arka.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 import com.arka.category.CategoryModuleConfig;
 import com.arka.notifications.NotificationsModuleConfig;
@@ -16,14 +14,6 @@ import com.arka.shopingCart.ShopingCartConfigurationModule;
 import com.arka.user.UserModuleConfig;
 
 @SpringBootApplication(scanBasePackages = "com.arka")
-@EntityScan(basePackages = "com.arka")  
-@EnableJpaRepositories(basePackages = "com.arka")
-@ComponentScan(basePackages = {
-	"com.arka",
-	"com.arka.notifications"
-
-} )
-
 @Import({
 	NotificationsModuleConfig.class,
 	OrderModuleConfig.class,
@@ -34,7 +24,6 @@ import com.arka.user.UserModuleConfig;
 	UserModuleConfig.class
 
 })
-
 public class ArkaAplication {
     public static void main(String[] args) {
 		SpringApplication.run(ArkaAplication.class, args);
---
+++ Backend/src/shared-kernel/src/testFixtures/java/com/arka/shared/domain/IntegrationTest.java
@@ -0,0 +1,13 @@
+package com.arka.shared.domain;
+
+import java.lang.annotation.ElementType;
+import org.junit.jupiter.api.Tag;
+import java.lang.annotation.Retention;  
+import java.lang.annotation.RetentionPolicy;  
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("integration")
+public @interface IntegrationTest {} 
