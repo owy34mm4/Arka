83813ebb282a3bbe3cd34b6a0fc3af08347c4378
a <a@a.com>
2026-03-08 01:18:46 -0500

    add :  ( E2E Tests | IntegrationTests ) : Se finaliza la suite de test el primer y mas pequeño modulo del aplicativo.
    Se generó la estructura base de reutilizacion para futuras suitesTest en otros modulos

--- Backend/src/category/src/main/java/com/arka/category/infrastructure/entryPoints/rest/dto/useCase/createCategory/RequestCreateCategory.java
+++ Backend/src/category/src/main/java/com/arka/category/infrastructure/entryPoints/rest/dto/useCase/createCategory/RequestCreateCategory.java
@@ -1,9 +1,11 @@
 package com.arka.category.infrastructure.entryPoints.rest.dto.useCase.createCategory;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class RequestCreateCategory {
     @NotBlank
     String name;
