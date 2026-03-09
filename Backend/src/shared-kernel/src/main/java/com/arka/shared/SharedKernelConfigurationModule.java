c414832b4ee8c3b39f6b4245592445fb60272e49
a <a@a.com>
2026-03-08 19:06:15 -0500

    catchup

--- Backend/src/shared-kernel/src/main/java/com/arka/shared/SharedKernelConfigurationModule.java
+++ Backend/src/shared-kernel/src/main/java/com/arka/shared/SharedKernelConfigurationModule.java
@@ -1,11 +1,12 @@
 package com.arka.shared;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.arka.shared")
-
+@EntityScan(basePackages = "com.arka.shared") 
 public class SharedKernelConfigurationModule {
     
 }
