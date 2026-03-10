b9876bfe99319531c96677d506ae0478eb62c246
a <a@a.com>
2026-03-10 17:16:05 -0500

    fix: RequestSerializationError: Spring serializa los json de request usando algun constructor publico que se deje abierto. No había y generaba error

--- Backend/src/category/src/main/java/com/arka/category/infrastructure/entryPoints/rest/dto/useCase/createCategory/RequestCreateCategory.java
+++ Backend/src/category/src/main/java/com/arka/category/infrastructure/entryPoints/rest/dto/useCase/createCategory/RequestCreateCategory.java
@@ -1,13 +1,21 @@
 package com.arka.category.infrastructure.entryPoints.rest.dto.useCase.createCategory;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import lombok.Setter;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class RequestCreateCategory {
     @NotBlank
     String name;
+    String x;
 
 }
